c7773818c3fcb756f9fb66ec895b64c811a92f60
Viacheslav Gurov <gurov>
2021-03-23 00:18:50 -0400

    dt 23 march 2021

---
+++ READ_ME.md
@@ -0,0 +1,24 @@
+hw_3_classes_if_for_dt_23_march_2021
+
+1: Did you play with if, for, class(es)?
+
+2: Are
+you
+ready
+to
+explain
+regarding
+classes, if, for
+23
+March
+2021
+at
+21: 00
+EST?
+
+3: Can
+you
+explain
+where
+above
+mentioned is used in programming?
---
+++ home_work_3_23_march_2021.py
@@ -0,0 +1,25 @@
+# 1. Write function for list of integers (create own) which returns list but with each element's index added to the itself.
+# E.g function_name([0, 1, 3, 5]) => [0, 2, 5, 8]
+
+# 2. Write function for list of elements (create own) that will return indices of all occurrences of that item in list
+# e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 'b') => [3, 4, 5]
+# e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 1) => [0, 6]
+# e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 'c') => []
+
+# 3. Create a function that takes an integer and returns its length.
+# e.g. function_name(8) => 1
+# e.g. function_name(88) => 2
+# e.g. function_name(83894) => 5
+# (Can't use len)
+
+# 4. Write a function that inverts the keys and values of a dictionary.
+# e.g. function_name({ 'a': 'b', 'c': 'd' }) => { 'b': 'a', 'd': 'c' }
+# e.g. function_name({ 'fruit': 'apple', 'meat': 'beef' }) => { 'apple': 'fruit', 'beef': 'meat' }
+
+# 5. Create class with attributes for last name and first name, full name and initials. Add functions to print full name, last name, first name, and initials
+# object_name = ClassName('john', 'DOE')
+# object_name.attribute_for_name => 'John Doe'
+# object_name.attribute_for_last_name => 'Doe'
+# object_name.attribute_for_first_name => 'John'
+# object_name.attribute_for_initials => 'J.D.'
+# object_name.print_full_name() => 'John Doe'
