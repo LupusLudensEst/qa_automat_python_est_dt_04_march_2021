eed7c71f4019477528c29cbcff3589d7ede41bf2
Viacheslav Gurov <gurov>
2021-03-23 23:42:40 -0400

    dt 23 march 2021

---
+++ home_work_3_dt_23_march_2021.py
@@ -0,0 +1,133 @@
+# hw_3_classes_if_for_dt_23_march_2021
+# 1. Write function for list of integers (create own) which returns list but with each element's index added to the itself.
+# E.g function_name([0, 1, 3, 5]) => [0, 2, 5, 8]
+
+print(f"1. Write function for list of integers (create own) which returns list but with each element's index added to the itself.\n"
+      f"E.g function_name([0, 1, 3, 5]) => [0, 2, 5, 8]")
+def ndx_ddd_itslf(m_lst):
+    for i, x in enumerate(m_lst):
+        print(f'Index "{i}" + its value "{x}" = "{i + x}"')
+
+print(ndx_ddd_itslf(list(range(-10, 10))))
+
+# 2. Write function for list of elements (create own) that will return indices of all occurrences of that item in list
+# e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 'b') => [3, 4, 5]
+# e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 1) => [0, 6]
+# e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 'c') => []
+
+print(f"""
+2. Write function for list of elements (create own) that will return indices of all occurrences of that item in list
+e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 'b') => [3, 4, 5]
+e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 1) => [0, 6]
+e.g. function_name([ 1, 2, 4, 'b', 'b', 'b', 1], 'c') => []
+""")
+def rtrn_indxs_of_ll_ccrncs(lst_2, trgt):
+    counter = list()
+    for i in list(range(0, len(lst_2))):
+        if lst_2[i] == trgt:
+            counter.append(i)
+    return counter
+
+print(f"""
+1. {rtrn_indxs_of_ll_ccrncs([ 1, 2, 4, 'b', 'b', 'b', 1], 'b')}
+2. {rtrn_indxs_of_ll_ccrncs([ 1, 2, 4, 'b', 'b', 'b', 1], 1)}
+3. {rtrn_indxs_of_ll_ccrncs([ 1, 2, 4, 'b', 'b', 'b', 1], 'c')}
+""")
+
+# 3. Create a function that takes an integer and returns its length.
+# e.g. function_name(8) => 1
+# e.g. function_name(88) => 2
+# e.g. function_name(83894) => 5
+# (Can't use len())
+
+print("""
+3. Create a function that takes an integer and returns its length.
+e.g. function_name(8) => 1
+e.g. function_name(88) => 2
+e.g. function_name(83894) => 5
+(Can't use len())
+""")
+
+intgr = str(int(input("Enter the integer: ")))
+def len_of_int(intgr):
+    counter = 0
+    for symbol in intgr:
+        counter += 1
+    return counter
+
+print(f'Length of "{intgr}" is "{len_of_int(intgr)}"')
+
+
+# 4. Write a function that inverts the keys and values of a dictionary.
+# e.g. function_name({ 'a': 'b', 'c': 'd' }) => { 'b': 'a', 'd': 'c' }
+# e.g. function_name({ 'fruit': 'apple', 'meat': 'beef' }) => { 'apple': 'fruit', 'beef': 'meat' }
+
+print("""
+4. Write a function that inverts the keys and values of a dictionary.
+# e.g. function_name({ 'a': 'b', 'c': 'd' }) => { 'b': 'a', 'd': 'c' }
+# e.g. function_name({ 'fruit': 'apple', 'meat': 'beef' }) => { 'apple': 'fruit', 'beef': 'meat' }
+""")
+
+dctnry = { 'a': 'b', 'c': 'd' }
+def ky_vl_invrtd(dctnry):
+    result = dict([(vl, ky) for ky, vl in dctnry.items()])
+    return result
+
+print(f"Input dictionary: {dctnry};\nOutput dictionary: {ky_vl_invrtd(dctnry)}")
+
+# 5. Create class with attributes for last name and first name, full name and initials. Add functions to print full name, last name, first name, and initials
+# object_name = ClassName('john', 'DOE')
+# object_name.attribute_for_name => 'John Doe'
+# object_name.attribute_for_last_name => 'Doe'
+# object_name.attribute_for_first_name => 'John'
+# object_name.attribute_for_initials => 'J.D.'
+# object_name.print_full_name() => 'John Doe'
+
+print("""
+5. Create class with attributes for last name and first name, full name and initials. 
+Add functions to print full name, last name, first name, and initials
+object_name = ClassName('john', 'DOE')
+object_name.attribute_for_name => 'John Doe'
+object_name.attribute_for_last_name => 'Doe'
+object_name.attribute_for_first_name => 'John'
+object_name.attribute_for_initials => 'J.D.'
+object_name.print_full_name() => 'John Doe'
+""")
+
+class ClassName:
+    """
+    Documentation for ClassName
+    """
+
+    def __init__(self, first_name, last_name, full_name, initials):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = full_name
+        self.initials = initials
+
+    def print_first_name(self):
+        # return self.first_name
+        print(f'First name: {first_name}')
+
+    def print_last_name(self):
+        # return self.last_name
+        print(f'Last name: {last_name}')
+
+    def print_full_name(self):
+        # return self.full_name
+        print(f'Full name: {full_name}')
+
+    def print_initials(self):
+        # return self.initials
+        print(f'Initials: {initials}')
+
+first_name = str(input("Enter the first name: "))
+last_name = str(input("Enter the last name: "))
+full_name = (f'{first_name} {last_name}')
+initials = (f'{first_name[0].upper()}.{last_name[0].upper()}.')
+
+object_1 = ClassName(first_name, last_name, full_name, initials)
+object_1.print_first_name()
+object_1.print_last_name()
+object_1.print_full_name()
+object_1.print_initials()
