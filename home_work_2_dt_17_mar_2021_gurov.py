70e459f3449defac43e4e635d426b40e36ad2e12
Viacheslav Gurov <gurov>
2021-03-25 22:40:27 -0400

    dt 25 march 2021

--- home_work_2_dt_17_mar_2021_gurov.py
+++ home_work_2_dt_17_mar_2021_gurov.py
@@ -1,18 +1,6 @@
-"""
-Week 2, 17 March 2021
-1. Write a Python program to print a specified list after removing the 0th, 4th, and 5th elements.
-2. Write a Python program to generate and print a list except for the first 5 elements
-3. Write a Python program to assign a number to a and b and print the result of sum, subtraction, multiplication, division
-4. Write a Python program to assign a number to either a or b and another should be a string
-(you can decide which one is string and which one is integer or assume that they can be different type every run)
-and print the result of the multiplication. Also, print change both to string and print out “a + any word + b” 10 times where a and b are your values.
-5. Write a Python program to print a list of integers without minimum and maximum values
-6. Write a Python program to sort and then print reverse list: ['b', 'n', 'A', 'g', 'S', 'p', 'm', 'r', 'R', 'X', 'z', 'B', 'I', 'H', 'A', 'e', 't', 'k', 'k', 'k', 'l', 'c', 'g', 'S', 'P', 'q', 'Y', 'Q']
-"""
-
 print(f'Homework_2, Week 2, 17 March 2021.\n1. Write a Python program to print a specified list after removing the 0th, 4th, and 5th elements.')
 list_1 = [1, 2, 3, 4, 'String', [5, 6, 7, 8], 'Alfa', 'Beta']
-print(f'List_1 before removing: {list_1}')
+print(f'List_1 before removing with indexes: {list(enumerate(list_1))}')
 del list_1[5]
 print(f'List_1 after removing 5th element: {list_1}')
 del list_1[4]
