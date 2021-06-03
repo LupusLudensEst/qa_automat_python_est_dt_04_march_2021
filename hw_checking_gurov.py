0da1c6f9a6f2df91ba36c728f28f66f264f7f7be
Viacheslav Gurov <gurov>
2021-06-02 20:49:52 -0400

    dt 02 June 2021

--- hw_checking_gurov.py
+++ hw_checking_gurov.py
@@ -5,7 +5,7 @@ print(x is y) # output True because "y" just has value of list_of_strings
 print(f'x: {x}, y: {y}')
 
 y = ["apple", "banana", "cherry"]
-y == x # there is a comparasion,thus "y" is not equal to "x", "y" and "x" are different objects
+y == x # there is a comparison,thus "y" is not equal to "x", "y" and "x" are different objects
 print(x is y)
 print(f'x: {x}, y: {y}')
 
